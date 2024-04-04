3d34b76d14ebc3c0b633d416fe02f6182818265a
Ivan Nikitin <ivan@niktivan.org>
2024-04-05 00:47:38 +0100

    Add logging and error handling to server

--- flask/app.py
+++ flask/app.py
@@ -1,114 +1,67 @@
 import io
 import os
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-import pandas as pd
-from datetime import datetime
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+from flask_cors import CORS
 from dotenv import load_dotenv
 from base64 import encodebytes
 from PIL import Image
+from log import RequestLogger, ServerLoadLogger
+from plot import get_exercises, get_info, plot_all
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+request_logger = RequestLogger()
+load_logger = ServerLoadLogger()
+
+@app.before_request
+def log_request_info():
+    request_logger.log_request(request)
+
+@app.before_first_request
+def start_load_logging():
+    load_logger.start_logging()
+
+MAX_FILE_SIZE_BYTES = 10 * 1024 * 1024  # 10 MB
+
 CORS(app)
 
 root = os.path.dirname(__file__)
 
-def get_info(file):
-    df = pd.read_csv(file)
-    df = df.rename(columns={"Exercise" : "Exercise Name"})
-    return df
-
-def get_exercises(file, date_format, info):
-    df = pd.read_csv(file, sep=';')
-    df = df[['Date', 'Exercise Name', 'Weight', 'Reps']]
-    # TODO: consider handling different weight units
-    df['Date'] = df['Date'].apply(lambda x: datetime.strptime(x, date_format).date())
-    df['Volume'] = df['Weight'] * df['Reps']
-
-    result = pd.merge(df, info, how="left", on="Exercise Name")
-    return result
-
-def plot_volumes(data, filename):
-    fig, ax = plt.subplots()
-    ax = sns.lineplot(data=data, x="Date", y="Volume", label="Volume", marker="H", lw=2.5, ls="--")
-    title = filename.split("/")[-1]
-    ax.set(ylabel="Volume (kg)", title=title)
-    sns.move_legend(ax, "upper left") # set volume legend to upper left
-
-    ax2 = ax.twinx()
-    ax2= sns.lineplot(data=data, x="Date", y="Reps", ax=ax2, label="Reps", color="r", marker="*", lw=1, ls=":")
-    ax2.set(ylabel="Reps")
-    sns.move_legend(ax2, "upper right") # set reps legend to upper right
-    
-    fig.autofmt_xdate()
-    plt.savefig(filename+".png")
-    plt.close("all")
-
-def plot_sets(primary, secondary, mrv, mev, filename):
-    fig, ax = plt.subplots()
-    sns.lineplot(ax=ax, data=primary, x="Date", y="Sets", label="Primary", marker="H")
-    sns.lineplot(ax=ax, data=secondary, x="Date", y="Sets", label="Secondary", color="r", marker="p")
-    title = filename.split("/")[-1]
-    ax.set(title=title)
-    ax.axhline(y=mrv, label="MRV", color="green",ls=":")
-    ax.axhline(y=mev, label="MEV", color="green",ls=":")
-    fig.autofmt_xdate()
-    plt.savefig(filename+".png")
-    plt.close("all")
-
-def plot_all(data, directory):
-    #TODO: filter based on limit
-    
-    exercises = data["Exercise Name"].unique()
-    for ex in sorted(exercises):
-        all_sets = data[data["Exercise Name"]==ex]
-        all_sets = all_sets.groupby(["Date"]).agg({"Volume" : "sum", "Reps": "sum", "Date": "count"})
-        all_sets = all_sets.rename(columns={"Date" : "Sets"})
-        filename = directory + "/ex_" + ex
-        plot_volumes(all_sets, filename)
-
-
-    muscle_groups = list(data["Primary muscle group"].unique()) + list(data["Secondary muscle group"].unique()) + [np.nan]
-    muscle_groups = set(muscle_groups)
-    muscle_groups.remove(np.nan)
-    for mg in muscle_groups:
-        primary_sets = data[data["Primary muscle group"]==mg]
-        secondary_sets = data[data["Secondary muscle group"]==mg]
-        
-        mrv = primary_sets.iloc[0]["MRV"] if not primary_sets.empty else np.nan
-        mev = primary_sets.iloc[0]["MEV"] if not primary_sets.empty else np.nan
-
-        primary_sets = primary_sets.groupby(["Date"]).agg({"Date": "count"})
-        primary_sets = primary_sets.rename(columns={"Date" : "Sets"})
-        secondary_sets = secondary_sets.groupby(["Date"]).agg({"Date": "count"})
-        secondary_sets = secondary_sets.rename(columns={"Date" : "Sets"})
-
-        plot_sets(primary_sets, secondary_sets, mrv, mev, directory + "/mg_"+mg)
-
 @app.route('/get_gainz', methods=["POST"])
 def get_gainz():
+    
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+    
     file = request.files.get('file')
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    if len(file.read()) > MAX_FILE_SIZE_BYTES:
+        return jsonify({'error': f'File size exceeds maximum allowed size of {MAX_FILE_SIZE_BYTES} bytes'}), 400
+    
     exercises_info = get_info(os.path.join(root, "exercises.csv"))
-    exercises = get_exercises(file, '%Y-%m-%d %H:%M:%S', exercises_info)
+    try:
+        exercises = get_exercises(file, '%Y-%m-%d %H:%M:%S', exercises_info)
+    except ValueError as e:
+        return jsonify({'error': e}), 400
+    
     plot_all(exercises, os.path.join(root, "./plots/"))
 
-    encoded_imges = []
+    encoded_images = []
     for _, _, files in os.walk(os.path.join(root, "./plots")):
         for file_name in files:
-            encoded_imges.append(get_response_image(os.path.join(root, "./plots/" + file_name)))
+            encoded_images.append(_get_response_image(os.path.join(root, "./plots/" + file_name)))
             os.remove(os.path.join(root, "./plots/" + file_name))
-    response = jsonify(encoded_imges)
+    response = jsonify(encoded_images)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-def get_response_image(image_path):
+def _get_response_image(image_path):
     pil_img = Image.open(image_path, mode='r')
     byte_arr = io.BytesIO()
     pil_img.save(byte_arr, format='PNG')
---
+++ flask/log.py
@@ -0,0 +1,29 @@
+import logging
+import psutil
+import time
+
+class BaseLogger:
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(message)s')
+        file_handler = logging.FileHandler(f"{self.__class__.__name__.lower()}.log")
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+class RequestLogger(BaseLogger):
+    def log_request(self, request):
+        log_message = f"{request.method} {request.url}"
+        self.logger.info(log_message)
+
+class ServerLoadLogger(BaseLogger):
+    def log_server_load(self):
+        cpu_percent = psutil.cpu_percent()
+        memory_percent = psutil.virtual_memory().percent
+        log_message = f"CPU Usage: {cpu_percent}% | Memory Usage: {memory_percent}%"
+        self.logger.info(log_message)
+        
+    def start_logging(self, interval=600):
+        while True:
+            self.log_server_load()
+            time.sleep(interval)
