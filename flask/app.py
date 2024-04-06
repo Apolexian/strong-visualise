1e00f0ba2bfec8389848af65c01a66bafb404f2d
Ivan Nikitin <ivan@niktivan.org>
2024-04-06 06:45:20 +0100

    Fix git

--- flask/app.py
+++ flask/app.py
@@ -35,6 +35,7 @@ root = os.path.dirname(__file__)
 def default():
     return jsonify('OK'), 200
 
+# Webhook updates pythonanywhere repo
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
